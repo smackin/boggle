a9a3f3ab6a9f4999a08098b1e0905a9a206e7923
Stephanie <smmackin12@gmail.com>
2022-03-05 18:23:47 -0500

    added score counter, response display and new css

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-import pdb
 from flask import Flask, render_template, request, session, jsonify
 from boggle import Boggle
+import pdb
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top_secret'
 
 boggle_game = Boggle()
 #instance of Boggle() 
-
 
 @app.route('/')
 def show_main_page(): 
@@ -17,13 +16,13 @@ def show_main_page():
     session['board'] = board
     return render_template('index.html', board=board)
 
-
 @app.route('/check-word')
 def check_word():
     """take the word from the params of the axios.GET request and check it against the board we have saved in the session."""
     word = request.args['word']
     board = session['board']
     response_string = boggle_game.check_valid_word(board, word)
+    # response_String takes the boggle_game variable( which is a new insance of boggle in boggle.py ) which is using the class method of check_valid_word and accepting board (with letters) and word (from the input) 
     # this variable will represent one of the three strings in check_valid_word function. 
     return jsonify({'response': response_string})
     # this route is for running the check_valid_word() function which is an AJAX request and uses JSON.  this is why the jsonify method is used.   we need to make the response, which is a dictionary, and return it as JSON in order to process the data sent to the request.  
--- static/app.js
+++ static/app.js
@@ -2,6 +2,8 @@
 
 let score = 0; 
 
+let words = []; 
+
 // create a form variable that selects the form from the html by class and add an event listener to prevent the page from reloading once the form is submitted.  
 let form = document.getElementById('form');
 form.addEventListener('submit', async function(e){
@@ -15,13 +17,23 @@ form.addEventListener('submit', async function(e){
     // send get request with axios to '/check-word' route with params passed in 
     let res = await axios.get('/check-word', { params: { word: word } }); 
     
-    console.log(res)
+    //console.log(res)
    
     let response = res.data.response; 
     console.log(response);
 // append response to p id = response 
-let paragraph = document.getElementById("response");
-paragraph.textContent = response;
+    let paragraph = document.getElementById("response");
+    paragraph.textContent = response;
 //reset form
-form.reset();
+    form.reset();
+
+    // score initialized to 0 - write function - if word is 'ok' then add word.length to the score.  and display it in the DOM.   
+
+    if (response === 'ok' ) {
+        console.log(response)
+        score += word.length; 
+        let addScore = document.getElementById("score");
+        addScore.textContent = ` Your score is ...  ${score}`;  
+    };
 })
+
